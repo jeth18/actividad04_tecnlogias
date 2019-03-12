f5c5f6d6bd039442b1387359b35ad33069198119
ang0823 <angelsanchez934@gmail.com>
2019-03-11 18:07:12 -0600

    Se corrigio el problema de la fecha

--- actividad04_tecnlogias/TCS20191_Actividad_04/src/main/java/com/rcolorado/tcs20191/actividad04/Main.java
+++ actividad04_tecnlogias/TCS20191_Actividad_04/src/main/java/com/rcolorado/tcs20191/actividad04/Main.java
@@ -13,11 +13,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
-
-
 
 /**
  *
@@ -25,75 +24,73 @@ import java.util.Scanner;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException  {
+  public static void main(String[] args) throws IOException {
 
-        Date date = new Date();
-        Scanner scanner = new Scanner(System.in);
-        String divisa;
-        do {
-        System.out.println("********************");
-        System.out.println("¿Cuánto vale un Bitcoin en mi divisa?");
-        System.out.println("********************");
-        System.out.println("Introduce el nombre de tu divisa (MXN, USD, CAD, EUR, ARS, VES):");
+    Scanner scanner = new Scanner(System.in);
+    String divisa;
+    do {
+      System.out.println("********************");
+      System.out.println("¿Cuánto vale un Bitcoin en mi divisa?");
+      System.out.println("********************");
+      System.out.println("Introduce el nombre de tu divisa (MXN, USD, CAD, EUR, ARS, VES):");
 
-        divisa = scanner.next();
-        System.out.println("... espere un momento");
+      divisa = scanner.next();
+      System.out.println("... espere un momento");
 
-        List<DivisaJsonClass> lista = ConsultaBitCoinMarket();
+      List<DivisaJsonClass> lista = ConsultaBitCoinMarket();
 
-        System.out.println("Se consultó de un total de : " + lista.size() + " divisas");
+      System.out.println("Se consultó de un total de : " + lista.size() + " divisas");
 
-        boolean encontrado = false;    //Indica si se encontró la divisa solicitada.
+      boolean encontrado = false;    //Indica si se encontró la divisa solicitada.
 
-        for (int i  = 0; i < lista.size(); i++) {
+      for (int i = 0; i < lista.size(); i++) {
         
-            if (lista.get(i).symbol.equals("localbtc" + divisa.toUpperCase())) {
-                System.out.println(lista.get(i).currency + " : " + lista.get(i).ask);
-                System.out.println("Venta : " + lista.get(i).bid);
-                System.out.println(date.getDate() + "/" + date.getMonth() + "/" + "2019");
-                encontrado = true;
-            }
+        if (lista.get(i).symbol.equals("localbtc" + divisa.toUpperCase())) {
+          System.out.println(lista.get(i).currency + " : " + lista.get(i).ask);
+          System.out.println("Venta : " + lista.get(i).bid);
+          System.out.print(LocalDate.now().getDayOfMonth() + "/");
+          System.out.print(LocalDate.now().getMonth() + "/");
+          System.out.println(LocalDate.now().getYear());
+          
+          encontrado = true;
         }
-        if (!encontrado) {
-            System.out.println("No se pudo encontrar la divisa solicitada.");
-        }
-        }while(!divisa.equals("NINGUNO"));
+      }
+      if (!encontrado) {
+        System.out.println("No se pudo encontrar la divisa solicitada.");
+      }
+    } while (!divisa.equalsIgnoreCase("NINGUNO"));
 
+  }
 
-    }
-    
-    
-    
-    private static List<DivisaJsonClass> ConsultaBitCoinMarket() throws IOException {
-        URL url = new URL("http://api.bitcoincharts.com/v1/markets.json");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Accept", "application/json");
+  private static List<DivisaJsonClass> ConsultaBitCoinMarket() throws IOException {
+    URL url = new URL("http://api.bitcoincharts.com/v1/markets.json");
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.setRequestMethod("GET");
+    conn.setRequestProperty("Accept", "application/json");
 
-        if (conn.getResponseCode() != 200) {
-            throw new RuntimeException("Fallo : HTTP error code : " + conn.getResponseCode());
-        }
-
-        InputStreamReader isr = new InputStreamReader(conn.getInputStream());
-        TypeToken<List<DivisaJsonClass>> token = new TypeToken<List<DivisaJsonClass>>() {
-        };
-        List<DivisaJsonClass> lista = new Gson().fromJson(isr, token.getType());
-        
-        return lista;
+    if (conn.getResponseCode() != 200) {
+      throw new RuntimeException("Fallo : HTTP error code : " + conn.getResponseCode());
     }
 
-            
-    /*
+    InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+    TypeToken<List<DivisaJsonClass>> token = new TypeToken<List<DivisaJsonClass>>() {
+    };
+    List<DivisaJsonClass> lista = new Gson().fromJson(isr, token.getType());
+
+    return lista;
+  }
+
+  /*
      * Clase privada para obtener los datos de las divisas
      *
-     */
-            
-    class DivisaJsonClass {
-        // Precio a la venta
-        public double bid;
-        public String currency;
-        // Precio a la compra
-        public double ask;
-        public String symbol;
-    }
+   */
+  class DivisaJsonClass {
+    // Precio a la venta
+
+    public double bid;
+    public String currency;
+    // Precio a la compra
+    public double ask;
+    public String symbol;
+  }
 }
